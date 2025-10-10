06981a74275f41a2a8ccad9961686e0b97f40400
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2025-10-10 13:22:36 +0900

    update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
-    const docPath = path.join(__dirname, 'src', 'views', 'documentation.html');
+    const docPath = path.join(__dirname, 'documentation.html');
 
     fs.readFile(docPath, 'utf8', (err, html) => {
         if (err) {
