dfa34a27a9f7e950290c200a4464d75b142c4a7c
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2025-10-11 23:06:41 +0900

    update

---
+++ constants.js
@@ -0,0 +1,9 @@
+/**
+ * Batas penggunaan untuk pengguna tamu (guest).
+ */
+export const GUEST_USAGE_LIMIT = 3;
+
+/**
+ * Batas ukuran file unggahan dalam bytes (5 MB).
+ */
+export const MAX_FILE_SIZE = 5 * 1024 * 1024;
