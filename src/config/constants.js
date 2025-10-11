ae5c6ae07e3a4020e807abf673253936507b0a5a
Christ Henry Loha <62457928+CristLoha@users.noreply.github.com>
2025-10-11 23:10:29 +0900

    update

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,2 @@
 export const GUEST_USAGE_LIMIT = 3;
+export const MAX_FILE_SIZE = 5 * 1024 * 1024;
--- src/controllers/constants.js
+++
@@ -1,9 +0,0 @@
-/**
- * Batas penggunaan untuk pengguna tamu (guest).
- */
-export const GUEST_USAGE_LIMIT = 3;
-
-/**
- * Batas ukuran file unggahan dalam bytes (10 MB).
- */
-export const MAX_FILE_SIZE = 5 * 1024 * 1024;
